b8f1aa028406629d11fd79ba064b3d7ee572888d
priyadarshi035 <priypete@in.ibm.com>
2016-10-26 10:33:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var http= require('https');
-var username = '85686c07-cb41-4aa1-a121-56a9dae74828-bluemix';
-var apiToken = 'afa70fc4e03c2fd97a1efc68a6509157b180e11f0066022b8604259462d163bf';
+var username = '';
+var apiToken = '';
 var buffer = new Buffer(username + ':' + apiToken);
 var base64String = buffer.toString('base64');
 var authorization = 'Basic ' + base64String ;
@@ -10,7 +10,7 @@ var response;
 var options = {
 	headers:{'Authorization': authorization},
    	method:'GET',
-	host:'85686c07-cb41-4aa1-a121-56a9dae74828-bluemix.cloudant.com',
+	host:'',
 	path:'/test/08825A'
 };
 http.request(options, function(res) {
